6a208a0f27fbc947871e30879f28a5861f114d18
David Dugle <david.dugleii@yahoo.com>
2020-11-12 16:17:25 -0500

    set up some of the links and file folders for the work. Grid is completed as a mock up. header is set

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Weather-Dashboard
+
+got the initial setup and file folders straight
+set up a grid for the project
+pseudo code
+banner set up along with the css behind it
+
+
+
+
+
+
+
+
+
+Photos
+![First Photo]()
---
+++ assets/JavaScript/script.js
@@ -0,0 +1,26 @@
+//going to go ahead and use moment to get the date
+
+
+
+
+//let's start with getting the input box information and appending buttons/make sure we are using local storage/
+
+
+
+
+
+
+
+//this section for today's weather
+
+
+
+
+
+
+
+
+
+
+
+//this section is for the 
