1c95ef5e8ccf310089c25c6b02ff9b090f42ef26
David Dugle <david.dugleii@yahoo.com>
2020-11-12 21:25:19 -0500

    making a lot of progress. the weather side is starting to come into shape.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 -making good progress, got the buttons to populate correctly dynamically
 -got the html set up 
 -local storage like a BOSS!
+-making a lot of progress with the weather side of it.have the current set up and need to get to the UV index. I am saving the icons for later.
 
 
 
--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -13,13 +13,15 @@ var citySrch = [];
 
 //this is going to send the button with the name of the city to the correct area
 
-function renderButtons(){
+function renderButtons() {
     $("#prev-search").empty();
-    
-    var storedCities=JSON.parse(localStorage.getItem("citySrch"));
 
-    for (var i=0;i<storedCities.length;i++){
-        var a=$("<button>");
+    var storedCities = JSON.parse(localStorage.getItem("citySrch"));
+    if (storedCities !== null) {
+        citySrch = storedCities
+    }
+    for (var i = 0; i < storedCities.length; i++) {
+        var a = $("<button>");
         a.addClass("city");
         a.attr("data-name", storedCities[i]);
         a.text(storedCities[i]);
@@ -29,25 +31,49 @@ function renderButtons(){
 
 };
 //this is going to set up a function for the button
-$("#srch-but").on("click", function(event){
+$("#srch-but").on("click", function (event) {
     event.preventDefault();
-    var srchCity =$("#city-search").val().trim();
+    var srchCity = $("#city-search").val().trim();
     citySrch.push(srchCity);
     localStorage.setItem("citySrch", JSON.stringify(citySrch));
+    $("#city-search").val("");
+
+
     renderButtons();
-    
+
 
 })
 renderButtons();
 
 
-
-
-
-
 //this section for today's weather
 
 
+function getWeather(){
+    var getWeath =$(this).attr("data-name");
+    var APIKey = "a0bebcdf3f3421ef2b632b830fa93f18";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + getWeath +"&appid=" + APIKey;
+    
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) { 
+
+        console.log(response);
+        $("#city-date").text(response.name);
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $("#curr-temp").text("Current Temp: "+tempF +" F");
+        $("#humidity").text("Current Humidity: "+ response.main.humidity +"%");
+        $("#wind-speed").text("Current Wind Speed: "+ response.wind.speed);
+
+
+    });
+
+
+
+
+};
 
 
 
@@ -55,6 +81,6 @@ renderButtons();
 
 
 
+$(document).on("click", ".city", getWeather);
 
-
-//this section is for the 
+//this section is for the 5 day forcast
