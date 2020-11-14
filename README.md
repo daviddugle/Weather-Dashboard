31568351705949229303c6fe7d109a8543f8aebb
David Dugle <david.dugleii@yahoo.com>
2020-11-13 20:45:58 -0500

    got the icons working like they should, may have a couple items to tweak.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Weather-Dashboard
 
+This was a super challenging exercise. I felt like the best aveneue for me to approach it would be to attack small sections at a time. Pretty proud of how it turned out and my problem solving abilities.
+
 -got the initial setup and file folders straight
 -set up a grid for the project
 -pseudo code
@@ -10,6 +12,7 @@
 -making a lot of progress with the weather side of it.have the current set up and need to get to the UV index. I am saving the icons for later.
 -have the five day forcast about straight. the icon on the main page is working and need to do the same on the five day
 -got the uv index working correctly.  had problems with the order of operation and once I figured that out it was pretty smooth sailing.
+-got the icons sorted out now for the five day
 
 
 
@@ -23,4 +26,5 @@ https://daviddugle.github.io/Weather-Dashboard/
 Photos
 ![First Photo](https://github.com/daviddugle/Weather-Dashboard/blob/main/assets/Photos/grid%20layout.jpg)
 ![Second Photo](https://github.com/daviddugle/Weather-Dashboard/blob/main/assets/Photos/Screenshot%202020-11-13%20161820.jpg)
-![Third Photo]()
+![Third Photo](https://github.com/daviddugle/Weather-Dashboard/blob/main/assets/Photos/Screenshot%202020-11-13%20200851.jpg)
+![Fourth Photo]()
--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -104,7 +104,7 @@ function getWeather() {
         
         indexUV();
         
-
+        //got the uv index in this area right here.pretty proud of my if statements    
         function indexUV(){
 
                
@@ -165,51 +165,48 @@ function getWeather() {
                 //day one
 
                 $("#day1-date").text(response.list[10].dt_txt);
-                // console.log(response.list[1]);
-                // var iconIns1 = (response.list[10].weather[0].icon);
-                // var icon1 = "http://openweathermap.org/img/wn/" + iconIns + "@2x.png";
-                // $("$day1-icon").attr("src", icon1);
+                var iconIns1 = (response.list[10].weather[0].icon);
+                var icon1 = "http://openweathermap.org/img/wn/" + iconIns1 + "@2x.png";                
+                $("#day1-icon").attr("src", icon1);
                 var tempF1 = (Math.floor(response.list[10].main.temp - 273.15) * 1.80 + 32).toFixed(2);
                 $("#day1-temp").text("Temp: " + tempF1 + "F");
                 $("#day1-humidity").text("Humidity:" + response.list[10].main.humidity + "%");
 
                 //day two                
                 $("#day2-date").text(response.list[18].dt_txt);
-                // console.log(response.list[1]);
-                // var iconIns1 = (response.list[1].weather[0].icon);
-                // var icon1 = "http://openweathermap.org/img/wn/" + iconIns + "@2x.png";
-                // $("$day1-icon").attr("src", icon1);
+                
+                var iconIns2 = (response.list[18].weather[0].icon);
+                var icon2 = "http://openweathermap.org/img/wn/" + iconIns2 + "@2x.png";
+                console.log(icon2);
+                $("#day2-icon").attr("src", icon2);
                 var tempF2 = (Math.floor(response.list[18].main.temp - 273.15) * 1.80 + 32).toFixed(2);
                 $("#day2-temp").text("Temp: " + tempF2 + "F");
                 $("#day2-humidity").text("Humidity:" + response.list[18].main.humidity + "%");
 
                 //day three                
-                $("#day3-date").text(response.list[26].dt_txt);
-                // console.log(response.list[1]);
-                // var iconIns1 = (response.list[1].weather[0].icon);
-                // var icon1 = "http://openweathermap.org/img/wn/" + iconIns + "@2x.png";
-                // $("$day1-icon").attr("src", icon1);
+                $("#day3-date").text(response.list[26].dt_txt);                
+                var iconIns3 = (response.list[26].weather[0].icon);
+                var icon3 = "http://openweathermap.org/img/wn/" + iconIns3 + "@2x.png";
+                $("#day3-icon").attr("src", icon3);
                 var tempF3 = (Math.floor(response.list[26].main.temp - 273.15) * 1.80 + 32).toFixed(2);
                 $("#day3-temp").text("Temp: " + tempF3 + "F");
                 $("#day3-humidity").text("Humidity:" + response.list[26].main.humidity + "%");
 
 
                 //day four                
-                $("#day4-date").text(response.list[34].dt_txt);
-                // console.log(response.list[1]);
-                // var iconIns1 = (response.list[1].weather[0].icon);
-                // var icon1 = "http://openweathermap.org/img/wn/" + iconIns + "@2x.png";
-                // $("$day1-icon").attr("src", icon1);
+                $("#day4-date").text(response.list[34].dt_txt);                
+                var iconIns4 = (response.list[34].weather[0].icon);
+                var icon4 = "http://openweathermap.org/img/wn/" + iconIns4 + "@2x.png";
+                $("#day4-icon").attr("src", icon4);
                 var tempF4 = (Math.floor(response.list[34].main.temp - 273.15) * 1.80 + 32).toFixed(2);
                 $("#day4-temp").text("Temp: " + tempF4 + "F");
                 $("#day4-humidity").text("Humidity:" + response.list[34].main.humidity + "%");
 
-                //day five                
-                $("#day5-date").text(response.list[39].dt_txt);
-                // console.log(response.list[1]);
-                // var iconIns1 = (response.list[1].weather[0].icon);
-                // var icon1 = "http://openweathermap.org/img/wn/" + iconIns + "@2x.png";
-                // $("$day1-icon").attr("src", icon1);
+                //day five               
+                $("#day5-date").text(response.list[39].dt_txt);                
+                var iconIns5 = (response.list[39].weather[0].icon);
+                var icon5 = "http://openweathermap.org/img/wn/" + iconIns5 + "@2x.png";
+                $("#day5-icon").attr("src", icon5);
                 var tempF5 = (Math.floor(response.list[39].main.temp - 273.15) * 1.80 + 32).toFixed(2);
                 $("#day5-temp").text("Temp: " + tempF5 + "F");
                 $("#day5-humidity").text("Humidity:" + response.list[39].main.humidity + "%");
